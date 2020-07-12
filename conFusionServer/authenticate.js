ef66b6f9de20782c55093a7f35df6e714aed572a
sutkarsh <aarushsingh3006@gmail.com>
2020-07-13 03:37:49 +0530

    Assgn 3

--- conFusionServer/authenticate.js
+++ conFusionServer/authenticate.js
@@ -1,7 +1,6 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('./models/user');
-
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
@@ -37,4 +36,35 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
         });
     }));
 
+exports.verifyUser = function (req, res, next) {
+        var token = req.body.token || req.query.token || req.headers['x-access-token'];
+        if (token) {
+            jwt.verify(token, config.secretKey, function (err, decoded) {
+                if (err) {
+                    var err = new Error('You are not authenticated!');
+                    err.status = 401;
+                    return next(err);
+                } else {
+                    req.decoded = decoded;
+                    next();
+                }
+            });
+        } else {
+            var err = new Error('No token provided!');
+            err.status = 403;
+            return next(err);
+        }
+    };
+
+exports.verifyAdmin = function (req, res, next) {
+        if (req.user.admin) {
+            next();
+        } else {
+            var err = new Error('You are not authorized to perform this operation!');
+            err.status = 403;
+            return next(err);
+        }
+    }
+
+  
 exports.verifyUser = passport.authenticate('jwt', {session: false});
