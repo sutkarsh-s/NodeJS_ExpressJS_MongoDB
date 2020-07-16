1c967f872e56e9f727acef8389409e9cd34d9c4e
sutkarsh <aarushsingh3006@gmail.com>
2020-07-16 15:39:03 +0530

    Integrating React Client and Server

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -16,6 +16,7 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 const uploadRouter = require('./routes/uploadRouter');
 var favouriteRouter = require('./routes/favouriteRouter');
+var commentRouter = require('./routes/commentRouter');
 
 const mongoose = require('mongoose');
 
@@ -67,6 +68,7 @@ app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 app.use('/imageUpload',uploadRouter);
 app.use('/favourites', favouriteRouter);
+app.use('/comments',commentRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
